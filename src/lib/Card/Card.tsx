d31e7f20980dfcc3858ffa9491753cb707daed71
Captain Mango <captainmango@code423n4.com>
2023-09-27 10:21:25 -0700

    Add documentation for the component

--- src/lib/Card/Card.tsx
+++ src/lib/Card/Card.tsx
@@ -5,6 +5,27 @@ import { Button } from "../Button";
 import { ButtonSize, ButtonType, ButtonVariant } from "../Button/Button.types";
 import "./Card.scss";
 
+/**
+ * A stylized Code4rena card with 4 variants and optional footer, cta button, and title.
+ * Requires an image tag for the top left corner and children for the body.
+ * 
+ * __Available variants:__
+ * - `TRANSPARENT` - makes the background of the card transparent
+ * - `OUTLINED` - adds border around the card
+ * - `COMPACT` - displays the body below the image to allow the card to be more narrow
+ * - `BOTTOM_ALIGNED` - makes the title and body bottom aligned instead of top aligned
+ * 
+ * @param children - Elements to be rendered inside the body of the card, passed in as children. 
+ * @param className - String of custom classes to extend the default styling of the component.
+ * @param variants - Array of style variants to be applied to the rendered component.
+ * @param footerDetails - Informational content to be rendered on the left side of the footer.
+ * @param footerLinks - Array of anchor elements to be rendered in the right side of the footer.
+ * @param imageSize - The size of the image displayed in the top corner. Can be small, medium or large. Defaults to medium
+ * @param image - Image element to be rendered in the top left corner
+ * @param imageBorderRadius - Border radius of the image. Can be circle, small, medium, or large. Defaults to circle.
+ * @param title - The title to be rendered at the top of the card body. Can be string or react node.
+ * @param cta - A call to action button rendered in the top right corner. Includes text and an onClick handler.
+ */
 export const Card: React.FC<CardProps> = ({
   children,
   className,
