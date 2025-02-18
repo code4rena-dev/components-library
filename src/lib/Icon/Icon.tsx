dc661a5053a02831aaeb72701d1cd87230238d2e
Nathan LaFreniere <nlf@code4rena.com>
2025-02-18 10:45:46 -0800

    chore: update link to icon gallery

--- src/lib/Icon/Icon.tsx
+++ src/lib/Icon/Icon.tsx
@@ -10,7 +10,7 @@ const getIcon = (name: string, size: string, color: string, className?: string) 
 /**
  * A Code4rena component for rendering any icon from the available Code4rena List.
  * For further details on all icon names, please visit the
- * [Icon Gallery](https://components-library-wine.vercel.app/?path=/docs/docs-iconography--docs)
+ * [Icon Gallery](https://code4rena-dev.github.io/components-library/?path=/docs/docs-iconography--docs)
  *
  * @param name - Icon name based on the available options.
  * @param size - One of three size options for the icon: small ("16px"), medium ("24px"), and large ("32px")
